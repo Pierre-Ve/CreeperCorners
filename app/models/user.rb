ad41e4fcb19e9d1002019d0723af2ead27e82e6e
pierre veillet <pierre.veillet38@gmail.com>
2023-11-21 14:32:26 +0100

    associations ajoutées sur les models build et user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  has_many :bookings, dependent: :destroy
+  has_many :builds, dependent: :destroy  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
