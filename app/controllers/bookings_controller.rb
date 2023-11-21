439c397a7257ce372e9721027f3964f2b5bbb523
pierre veillet <pierre.veillet38@gmail.com>
2023-11-21 15:20:06 +0100

    controlleur booking action create faite, creation form sur view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,13 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.build = Build.find(params[:build_id])
+    @booking.user = current_user
+    @booking.save
   end
 
   def destroy
@@ -12,5 +17,10 @@ class BookingsController < ApplicationController
   end
 
   def list
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:user, :build, :start_date)
   end
 end
