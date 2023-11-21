738f6ba0739af046d087696da9a0c0607d2f0b4c
Remi Lavigne <remilavigne.pro@gmail.com>
2023-11-21 13:57:54 +0100

    build_controller_action

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,19 +1,49 @@
 class BuildsController < ApplicationController
+  before_action :set_build, only: %i[ show edit update destroy ]
+
   def index
+    @builds = Build.all
+  end
+
+  def show
   end
 
   def new
+    @build = Build.new
   end
 
   def create
+    @build = Build.new(build_params)
+    if @build.save
+      redirect_to builds_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @build.update(build_params)
+      redirect_to build_path(@build)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @build.destroy
+    redirect_to builds_path, status: :see_other
+  end
+
+  private
+
+  def set_build
+    @build = Build.find(params[:id])
+  end
+
+  def build_params
+    params.require(:build).permit(:address, :surface, :description, :category, :price)
   end
 end
