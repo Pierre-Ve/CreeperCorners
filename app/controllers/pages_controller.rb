5e76447bd9adba3ccb220b9b012fa0eace9f4423
Fabien Meuret <fmeuret@gmail.com>
2023-11-23 17:37:29 +0100

    work in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,6 @@ class PagesController < ApplicationController
     @my_bookings = current_user.bookings
     @my_bookings = @my_bookings.where('start_date >= ? AND ( status = ? OR STATUS = ? )', Date.today, 0, 1 )
     @my_builds = current_user.builds
+    @my_builds_rented = Booking.joins(:build, :user).select("bookings.start_date, builds.id, status").where(" builds.user_id = ? AND (status = 0 OR status = 1)", current_user.id)
   end
 end
