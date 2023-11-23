923a3e5dccaf06dc376bb3b1663034f6405782ac
Fabien Meuret <fmeuret@gmail.com>
2023-11-23 09:27:03 +0100

    add on profile view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,7 @@ class PagesController < ApplicationController
 
   def my_profile
     @my_bookings = current_user.bookings
+    @my_bookings = @my_bookings.where('start_date >= ?', Date.today)
+    @my_builds = current_user.builds
   end
 end
